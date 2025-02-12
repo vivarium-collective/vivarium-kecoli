9ac9a4d368857eb9a62b0a9533e2c074c1626480
arnabmutsuddy <arnab.mutsuddy@gmail.com>
2025-02-12 12:07:54 -0500

    millard model executed

--- scripts/run_millard.py
+++ scripts/run_millard.py
@@ -21,3 +21,21 @@ ic_default = species_all.initial_concentration
 result_default = run_time_course(model=model_millard, duration=300)
 
 #%%
+
+# sp_plot = ["GLCx","ACE","G6P","F6P","GAP","PGA3","PYR","R5P","E4P","MAL","OAA","AKG","ACCOA"]
+
+sp_plot = ["GLCx","ACE","G6P","F6P","GAP","PYR","R5P","MAL","AKG","ACCOA"]
+
+
+fig, axs = plt.subplots(nrows=1, ncols=1, figsize=(8, 5))
+
+
+result = result_default
+for sp in sp_plot:
+    sp_traj = result.loc[:,sp].values
+    y_vals = sp_traj/max(sp_traj)
+    axs.plot(result.index, y_vals, label=sp)
+axs.legend(bbox_to_anchor=(0.95, 1))
+plt.show()
+
+#%%
