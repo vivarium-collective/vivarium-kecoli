26c863322c20636700378670952785f645dd4735
arnabmutsuddy <arnab.mutsuddy@gmail.com>
2025-02-17 12:23:18 -0500

    run_model perturb env updated

--- utils/run_model.py
+++ utils/run_model.py
@@ -1,25 +1,27 @@
 
+import basico
+
 
 def perturb_env(model_name,sp_name):
 
-    sp_all = list(get_species(model=model_name).index.values)
-    IC_all = get_species(model=model_name)["initial_concentration"].values
+    sp_all = basico.get_species(model=model_name).index.to_list()
+    IC_all = basico.get_species(model=model_name)["initial_concentration"].values
 
     sp_idx = sp_all.index(sp_name)
 
     sp_ic_default = IC_all[sp_idx]
 
-    result_default = run_time_course(model=model_name,duration=300)
+    result_default = basico.run_time_course(model=model_name,duration=300)
 
-    set_species(model=model_kecoli74, name=sp_name, initial_concentration=sp_ic_default / 10)
+    basico.set_species(model=model_name, name=sp_name, initial_concentration=sp_ic_default / 10)
 
-    result_low = run_time_course(model=model_name, duration=300)
+    result_low = basico.run_time_course(model=model_name, duration=300)
 
-    set_species(model=model_kecoli74, name=sp_name, initial_concentration=sp_ic_default*10)
+    basico.set_species(model=model_name, name=sp_name, initial_concentration=sp_ic_default*10)
 
-    result_high = run_time_course(model=model_name, duration=300)
+    result_high = basico.run_time_course(model=model_name, duration=300)
 
-    set_species(model=model_kecoli74, name=sp_name, initial_concentration=sp_ic_default)
+    basico.set_species(model=model_name, name=sp_name, initial_concentration=sp_ic_default)
 
     results_all = [result_default, result_low, result_high]
 
