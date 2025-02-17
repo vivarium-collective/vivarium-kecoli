3f4d066c1aa695a49ae13c0760da8e97d540b608
arnabmutsuddy <arnab.mutsuddy@gmail.com>
2025-02-17 13:24:07 -0500

    plotting aa

--- utils/plotting.py
+++ utils/plotting.py
@@ -20,3 +20,29 @@ def plot_pathway(results,sp_plot,labels,output_plots):
 
     plt.savefig(os.path.join(output_plots,'env_perturb.png'))
 
+def plot_aa(results,labels,output_plots):
+    sp_count = 0
+
+    sp_aa = ["Ala", "Arg", "Asn", "Asp", "Cys",
+             "Glu", "Gln", "Gly", "His", "Ile",
+             "Leu", "Lys", "Met", "Phe", "Pro",
+             "Ser", "Thr", "Trp", "Tyr", "Val"]
+
+
+    colors = ['blue', 'red', 'green']
+
+    fig, axs_aa = plt.subplots(nrows=4, ncols=5, figsize=(20, 15))
+
+    for ax_row in range(4):
+        for ax_col in range(5):
+            sp = sp_aa[sp_count]
+            for idx, res in enumerate(results):
+                sp_traj = res.loc[:, sp].values
+                axs_aa[ax_row, ax_col].plot(res.index, sp_traj, label=labels[idx], color=colors[idx])
+            axs_aa[ax_row, ax_col].set_xlabel('Time (s)')
+            axs_aa[ax_row, ax_col].set_ylabel(sp)
+            sp_count += 1
+    axs_aa[ax_row, ax_col].legend(loc='best')
+    plt.subplots_adjust(wspace=0.35)
+
+    plt.savefig(os.path.join(output_plots,'env_perturb_aa.png'))
