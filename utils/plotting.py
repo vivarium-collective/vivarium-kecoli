9e572ee8e6f2722902b03e42fa6c9100591a18ca
arnabmutsuddy <arnab.mutsuddy@gmail.com>
2025-02-17 14:57:36 -0500

    module cleanup

--- utils/plotting.py
+++ utils/plotting.py
@@ -3,52 +3,6 @@ import matplotlib.pyplot as plt
 plt.rcParams['figure.dpi'] = 90
 
 
-def plot_pathway_old(results,sp_plot,labels,output_plots):
-
-
-    fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(12, 4))
-
-
-    for ax_idx in range(3):
-        result = results[ax_idx]
-        for sp in sp_plot:
-            axs[ax_idx].plot(result.index, result.loc[:, sp].values, label=sp)
-
-        axs[ax_idx].set_xlabel('Time (s)')
-        axs[ax_idx].set_title(labels[ax_idx])
-    axs[ax_idx].legend(loc='best')
-
-    plt.savefig(os.path.join(output_plots,'env_perturb.png'))
-
-def plot_aa_old(results,labels,output_plots):
-    sp_count = 0
-
-    sp_aa = ["Ala", "Arg", "Asn", "Asp", "Cys",
-             "Glu", "Gln", "Gly", "His", "Ile",
-             "Leu", "Lys", "Met", "Phe", "Pro",
-             "Ser", "Thr", "Trp", "Tyr", "Val"]
-
-
-    colors = ['blue', 'red', 'green']
-
-    fig, axs_aa = plt.subplots(nrows=4, ncols=5, figsize=(20, 15))
-
-    for ax_row in range(4):
-        for ax_col in range(5):
-            sp = sp_aa[sp_count]
-            for idx, res in enumerate(results):
-                sp_traj = res.loc[:, sp].values
-                axs_aa[ax_row, ax_col].plot(res.index, sp_traj, label=labels[idx], color=colors[idx])
-            axs_aa[ax_row, ax_col].set_xlabel('Time (s)')
-            axs_aa[ax_row, ax_col].set_ylabel(sp)
-            sp_count += 1
-    axs_aa[ax_row, ax_col].legend(loc='best')
-    plt.subplots_adjust(wspace=0.35)
-
-    plt.savefig(os.path.join(output_plots,'env_perturb_aa.png'))
-
-
-#%%
 
 def plot_pathway(results,sp_plot,labels,output_plots):
 
--- utils/run_model.py
+++ utils/run_model.py
@@ -2,30 +2,6 @@
 import basico
 
 
-def perturb_env_old(model_name,sp_name):
-
-    sp_all = basico.get_species(model=model_name).index.to_list()
-    IC_all = basico.get_species(model=model_name)["initial_concentration"].values
-
-    sp_idx = sp_all.index(sp_name)
-
-    sp_ic_default = IC_all[sp_idx]
-
-    result_default = basico.run_time_course(model=model_name,duration=300)
-
-    basico.set_species(model=model_name, name=sp_name, initial_concentration=sp_ic_default / 10)
-
-    result_low = basico.run_time_course(model=model_name, duration=300)
-
-    basico.set_species(model=model_name, name=sp_name, initial_concentration=sp_ic_default*10)
-
-    result_high = basico.run_time_course(model=model_name, duration=300)
-
-    basico.set_species(model=model_name, name=sp_name, initial_concentration=sp_ic_default)
-
-    results_all = [result_default, result_low, result_high]
-
-    return results_all
 
 def perturb_env(model_name,sp_name):
 
