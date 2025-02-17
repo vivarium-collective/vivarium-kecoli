35474a57883ee4bca8d5b3ddc491e41aff38aed4
arnabmutsuddy <arnab.mutsuddy@gmail.com>
2025-02-17 13:49:21 -0500

    perturb env updated

--- utils/run_model.py
+++ utils/run_model.py
@@ -2,7 +2,7 @@
 import basico
 
 
-def perturb_env(model_name,sp_name):
+def perturb_env_old(model_name,sp_name):
 
     sp_all = basico.get_species(model=model_name).index.to_list()
     IC_all = basico.get_species(model=model_name)["initial_concentration"].values
@@ -24,5 +24,49 @@ def perturb_env(model_name,sp_name):
     basico.set_species(model=model_name, name=sp_name, initial_concentration=sp_ic_default)
 
     results_all = [result_default, result_low, result_high]
+
+    return results_all
+
+def perturb_env(model_name,sp_name):
+
+    sp_all = basico.get_species(model=model_name).index.to_list()
+    IC_all = basico.get_species(model=model_name)["initial_concentration"].values
+
+    sp_idx = sp_all.index(sp_name)
+
+    sp_ic_default = IC_all[sp_idx]
+
+    result_default = basico.run_time_course(model=model_name,duration=300)
+
+    basico.set_species(model=model_name, name=sp_name, initial_concentration=sp_ic_default / 10)
+
+    result_low = basico.run_time_course(model=model_name, duration=300)
+
+    basico.set_species(model=model_name, name=sp_name, initial_concentration=sp_ic_default*10)
+
+    result_high = basico.run_time_course(model=model_name, duration=300)
+
+    basico.set_species(model=model_name, name=sp_name, initial_concentration=sp_ic_default)
+
+
+
+    results_all = {}
+
+    results_all['baseline'] = {}
+
+    results_all['baseline']['result'] = result_default
+    results_all['baseline']['sp_perturb'] = sp_name
+    results_all['baseline']['sp_val'] = sp_ic_default
+
+    results_all['low'] = {}
+    results_all['low']['result'] = result_low
+    results_all['low']['sp_perturb'] = sp_name
+    results_all['low']['sp_val'] = sp_ic_default/10
+
+    results_all['high'] = {}
+    results_all['high']['result'] = result_high
+    results_all['high']['sp_perturb'] = sp_name
+    results_all['high']['sp_val'] = sp_ic_default*10
+
 
     return results_all
