8222d4826e1634fc0fa2c217ab289bf56edf647f
arnabmutsuddy <arnab.mutsuddy@gmail.com>
2025-03-03 10:32:53 -0500

    run query on webservice

--- scripts/biovelo_example.py
+++ scripts/biovelo_example.py
@@ -12,12 +12,9 @@ sys.path.append(wd)
 from utils.mapping import biocyc_credentials
 from utils.biovelo import *
 
-
-
 dir_credentials = os.path.join(wd,'credentials')
 
 s = biocyc_credentials(dir_credentials)
-
 
 
 #%%
@@ -34,5 +31,20 @@ substrates_r28 = ['CARBON-DIOXIDE', 'PHOSPHO-ENOL-PYRUVATE']
 products_r28 = ['OXALACETIC_ACID']
 
 expr_r28 = expr_rxns(substrates_r28,products_r28,db_name='ECOLI')
+
+#%%
+
+url_biovelo_query = "https://websvc.biocyc.org/xmlquery?query="
+
+response_r2x = s.get(url_biovelo_query + urllib.parse.quote(expr_r2x))
+response_r28 = s.get(url_biovelo_query + urllib.parse.quote(expr_r28))
+
+#%%
+print(response_r2x.text)
+
+
+#%%
+
+print(response_r28.text)
 
 #%%
