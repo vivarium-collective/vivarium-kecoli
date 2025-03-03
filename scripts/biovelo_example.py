3ea40bc2a8be4d905e690a8e0ab1cbd2bd1efc20
arnabmutsuddy <arnab.mutsuddy@gmail.com>
2025-03-03 10:26:42 -0500

    query example

--- scripts/biovelo_example.py
+++ scripts/biovelo_example.py
@@ -21,3 +21,18 @@ s = biocyc_credentials(dir_credentials)
 
 
 #%%
+
+substrates_r2x = ['FRUCTOSE-16-DIPHOSPHATE']
+products_r2x = ['FRUCTOSE-6P']
+
+expr_r2x = expr_rxns(substrates_r2x,products_r2x,db_name='ECOLI')
+
+
+#%%
+
+substrates_r28 = ['CARBON-DIOXIDE', 'PHOSPHO-ENOL-PYRUVATE']
+products_r28 = ['OXALACETIC_ACID']
+
+expr_r28 = expr_rxns(substrates_r28,products_r28,db_name='ECOLI')
+
+#%%
