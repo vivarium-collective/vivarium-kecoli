b6badd56f4bc874459612f3d2eada871d1386c05
arnabmutsuddy <arnab.mutsuddy@gmail.com>
2025-03-19 12:48:03 -0400

    KecoliCell initiated

---
+++ processes/kecoli_cell.py
@@ -0,0 +1,22 @@
+from vivarium.core.process import Process
+from vivarium.core.engine import Engine, pp
+from basico import *
+
+import os
+
+DEFAULT_MODEL_FILE = os.path.join('models','k-ecoli74.xml')
+
+class KecoliCell(Process):
+
+    defaults = {
+        'model_file': DEFAULT_MODEL_FILE,
+        'time_step': 1.0,
+    }
+
+    def __init__(self, parameters=None):
+        super().__init__(parameters)
+
+        self.copasi_model_object = load_model(self.parameters['model_file'])
+        all_species = get_species(model=self.copasi_model_object.index.tolist())
+
+    
